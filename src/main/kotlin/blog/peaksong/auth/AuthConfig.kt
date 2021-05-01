e4efc1745ac1266c119b3db130b6a418d45a1bb4
peeksong <18814122707@163.com>
2021-05-01 15:45:34 +0800

    finish

--- src/main/kotlin/blog/peaksong/auth/AuthConfig.kt
+++ src/main/kotlin/blog/peaksong/auth/AuthConfig.kt
@@ -1,25 +1,35 @@
 package blog.peaksong.auth
 
 import blog.peaksong.auth.core.AuthDecisionManager
+import blog.peaksong.auth.core.FilterMetadata
 import blog.peaksong.auth.core.MyPasswordEncoder
+import blog.peaksong.auth.handler.AuthDeniedHandler
+import blog.peaksong.auth.handler.AuthEntryPoint
+import blog.peaksong.base.WebConstant
 import blog.peaksong.util.LoggerDelegate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.security.config.annotation.ObjectPostProcessor
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource
+import blog.peaksong.auth.filter.AuthFilter
+import blog.peaksong.auth.filter.JwtFilter
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EntityScan("blog.peaksong.auth.model")
 @EnableJpaRepositories("blog.peaksong.auth.repository")
-class AuthConfig:  {
+class AuthConfig: WebSecurityConfigurerAdapter() {
 
     private val log by LoggerDelegate()
 
     @Autowired
-    private lateinit var FilterMetadata: FilterInvocationSecurityMetadataSource
+    private lateinit var filterMetadata: FilterMetadata
 
     @Autowired
     private lateinit var authDecisionManager: AuthDecisionManager
@@ -30,5 +40,41 @@ class AuthConfig:  {
     }
 
 
+    override fun configure(http: HttpSecurity) {
+        with(http) {
+            csrf.disable()
 
+            cors()
+
+            sessionManagement().disable()
+
+            formLogin().loginProcessingUrl(WebConstant.API_AUTH_URL).permitAll()
+            authorizeRequests().antMatchers("/").permitAll()
+
+            authorizeRequests().withObjectPostProcessor(
+                object: ObjectPostProcessor<FilterSecurityInterceptor> {
+                    override fun <O : FilterSecurityInterceptor?> postProcess(obj: O): O {
+                        obj!!.securityMetadataSource = filterMetadata
+                        obj.accessDecisionManager = authDecisionManager
+                        return obj
+                    }
+                }
+            )
+
+            authorizeRequests().anyRequest().authenticated()
+
+            exceptionHandling().accessDeniedHandler(AuthDeniedHandler())
+            exceptionHandling().authenticationEntryPoint(AuthEntryPoint())
+
+            addFilterBefore(
+                AuthFilter(authenticationManager()),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
+
+            addFilterBefore(JwtFilter(), AuthFilter::class.java)
+
+            log.info("HttpSecurity 配置完成")
+
+        }
+    }
 }
