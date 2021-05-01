85afd1f836a9e663e9ae3afb593c1e857cdcaa23
peeksong <18814122707@163.com>
2021-05-01 15:48:44 +0800

    Bug fix

--- src/main/kotlin/blog/peaksong/auth/model/UserRoleMap.kt
+++ src/main/kotlin/blog/peaksong/auth/model/UserRoleMap.kt
@@ -3,16 +3,16 @@ package blog.peaksong.auth.model
 import javax.persistence.*
 
 @Entity
-@IdClass(UserRoleMapPk::class.java)
-@Table("USER_ROLE_MAP", schema = "DEMO")
+@IdClass(UserRoleMapPk::class)
+@Table(name = "USER_ROLE_MAP", schema = "DEMO")
 data class UserRoleMap(
 
     @Id
-    @Column(name="USER_ID")
+    @Column(name = "USER_ID")
     val userId: String,
 
     @Id
-    @Column(name="ROLE_NAME")
+    @Column(name = "ROLE_NAME")
     val roleName: String
 
 )
